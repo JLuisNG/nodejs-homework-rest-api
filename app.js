584608d2362fdf24d994af44c9f7d7aa068780a1
Luis Nava <jorgex4p@gmail.com>
2024-03-11 00:00:28 -0500

    v4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const app = require('./app');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,14 +2,19 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
+// GET /api/contacts
 router.get('/', contactsController.listContacts);
 
+// GET /api/contacts/:id
 router.get('/:id', contactsController.getById);
 
+// POST /api/contacts
 router.post('/', contactsController.addContact);
 
+// DELETE /api/contacts/:id
 router.delete('/:id', contactsController.removeContact);
 
+// PUT /api/contacts/:id
 router.put('/:id', contactsController.updateContact);
 
 module.exports = router;
